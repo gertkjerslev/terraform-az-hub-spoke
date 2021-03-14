0f5ed253e59d482260432cd9f543a522468ecf71
Gert Kjerslev <gertk@v220.dk>
2021-03-14 11:54:11 +0100

    Update subnet i spoke1 vNet

--- network.tf
+++ network.tf
@@ -16,21 +16,21 @@ module "spoke1network" {
     resource_group_name = "homelab-${var.spoke1_vnet_name}-rg"
     location            = "westeurope"
     address_space       = "10.100.0.0/16"
-    subnet_prefixes     = ["10.100.1.0/24", "10.100.2.0/24"]
-    subnet_names        = ["spokesubnet1", "spokesubnet2"]
+    subnet_prefixes     = ["10.100.1.0/24"]
+    subnet_names        = ["spokesubnet1"]
 }
 
 
 # Peerings
 resource "azurerm_virtual_network_peering" "hubspoke1" {
-  name                      = "hubspoke1"
+  name                      = "hub_to_spoke1"
   resource_group_name       = "homelab-${module.hubnetwork.vnet_name}-rg"
   virtual_network_name      = module.hubnetwork.vnet_name
   remote_virtual_network_id = module.spoke1network.vnet_id
 }
 
 resource "azurerm_virtual_network_peering" "spoke1hub" {
-  name                      = "spoke1hub"
+  name                      = "spoke1_to_hub"
   resource_group_name       = "homelab-${module.spoke1network.vnet_name}-rg"
   virtual_network_name      = module.spoke1network.vnet_name
   remote_virtual_network_id = module.hubnetwork.vnet_id
